4270a50e8b1e6474c9f36ff1c34a6ec74399dec3
sugimoto <you@hpe.com>
2019-05-04 19:07:15 +0900

    change album's title dynamically

--- personal_album.js
+++ personal_album.js
@@ -1,7 +1,11 @@
+var member_list={iwai:'岩井', itabashi:'板橋', sugimoto:'杉本'};
+
 window.onload=function(){
     var personal_name=decodeURIComponent(location.search.split('=')[1]);
     // alert(personal_name);
-
+    var album_name=document.getElementById('album_name');
+    // console.log(album_name);
+    album_name.textContent=member_list[personal_name]+"'s Album";
 
     $('.active').removeClass('active');
     $('.'+personal_name).addClass('active');
