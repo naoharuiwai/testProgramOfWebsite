aefee4ee2873ba4b49f9e7060ade7c15d368de2b
sugimoto <you@hpe.com>
2019-05-04 18:35:02 +0900

    display specific personal album

--- personal_album.js
+++ personal_album.js
@@ -1,4 +1,8 @@
 window.onload=function(){
     var personal_name=decodeURIComponent(location.search.split('=')[1]);
     // alert(personal_name);
+
+
+    $('.active').removeClass('active');
+    $('.'+personal_name).addClass('active');
 }
